2011600634f7f18e68616f0e5df2fa0834f888e6
Unknown <fs.rodriguez91@gmail.com>
2018-09-20 11:57:14 -0300

    rutas mobile

--- src/app/public/public.routes.ts
+++ src/app/public/public.routes.ts
@@ -12,6 +12,10 @@ const publicRoutes: Routes = [
         children: [
             { path: 'home', component: HomeComponent, data: { titulo: 'Home' } },
             { path: 'blog', component: BlogComponent, data: { titulo: 'Blog' } },
+            { path: 'services',  redirectTo: '/home', pathMatch: 'full' },
+            { path: 'inbound',  redirectTo: '/home', pathMatch: 'full' },
+            { path: 'neuroturism',  redirectTo: '/home', pathMatch: 'full' },
+            { path: 'team',  redirectTo: '/home', pathMatch: 'full' },
             { path: '', redirectTo: '/home', pathMatch: 'full' }
         ]
     }
